8371ec7961410f3aca2572c09e3a807d030c365f
BurningHat20 <yashgohel395@gmail.com>
2024-02-21 19:33:56 +0530

    Sucess Fix

---
+++ src/components/Stripes.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Stripes() {
+  return (
+    <div className="max-w-72 py-3 border-t-2 border-b-2 border-r-2 border-zinc-700 relative bottom-16 "></div>
+  );
+}
+
+export default Stripes;
---
+++ src/components/Work.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from "react";
+
+function Work() {
+  var images = [
+    {
+      url: "http://tinyurl.com/2yrpawqn",
+      top: "62%",
+      left: "49%",
+      isActive: false,
+    },
+    {
+      url: "http://tinyurl.com/24k2l6rw",
+      top: "68%",
+      left: "46%",
+      isActive: false,
+    },
+    {
+      url: "http://tinyurl.com/22vacxoy",
+      top: "55%",
+      left: "52%",
+      isActive: false,
+    },
+    {
+      url: "http://tinyurl.com/2739arpv",
+      top: "70%",
+      left: "49%",
+      isActive: false,
+    },
+    {
+      url: "http://tinyurl.com/2apjuk35",
+      top: "62%",
+      left: "53%",
+      isActive: false,
+    },
+    {
+      url: "http://tinyurl.com/244vvnmt",
+      top: "58%",
+      left: "48%",
+      isActive: false,
+    },
+  ];
+  return (
+    <section className="w-full h-screen bg-zinc-900">
+      <div className="relative max-w-screen-xl mx-auto text-center">
+        <h1 className="text-[41vw] leading-none font-medium ">work</h1>
+        <div className="absolute top-0 w-full h-full ">
+          {images.map(
+            (elem, index) =>
+              elem.isActive && (
+                <img
+                  className="absolute w-[260px] rounded-xl -translate-x-[50%] -translate-y-[50%]"
+                  src={elem.url}
+                  style={{ top: elem.top, left: elem.left }}
+                  alt=""
+                />
+              )
+          )}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Work;
